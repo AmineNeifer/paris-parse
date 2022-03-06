34d12b6e61290563ee0ff059042eebf184c66643
AmineNeifer <AmineNeifer>
2022-03-06 18:54:38 +0100

    added the option to load data or not

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	activateModel()
+	var (
+		in        string
+		parsed_in string
+	)
+	fmt.Println("Do you want to remove all the data, re request it with the API and store it in database?")
+	fmt.Println("write yes to confirm, any other characters to deny")
+	fmt.Scan(&in)
+	parsed_in = strings.ToLower(strings.Trim(in, " "))
+	if parsed_in == "yes" {
+		activateModel()
+	} else {
+		fmt.Println("Great! no unnecessary stuff!")
+	}
+
 }
